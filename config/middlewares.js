11f51a17a328bea1e2811c889e0bca640a3ea351
celiamartinelli <celia.martinelli@oclock.school>
2024-06-20 09:15:27 +0200

    commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["http://localhost:19006", "http://192.168.1.73:19006"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
