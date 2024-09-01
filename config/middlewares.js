b5968f97a9f8e86ac1f0b8b77544ffb164f10977
celiamartinelli <celia.martinelli@oclock.school>
2024-09-01 17:27:45 +0200

    add

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,17 @@
-module.exports = [
+module.exports = ({ env }) => [
+  "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://textile-guide.netlify.app", "http://localhost:5173"], // Liste des origines autorisées
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
   "strapi::poweredBy",
-  "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
